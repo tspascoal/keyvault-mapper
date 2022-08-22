2f053275f68d3674c2f25c4bb8f4bff5a467ba75
Tiago Pascoal <tiago@pascoal.net>
2022-08-22 13:41:01 +0000

    Update external dependency.
    Disable eslint no english words rule

--- src/caseinsentivemap.ts
+++ src/caseinsentivemap.ts
@@ -2,7 +2,7 @@ export class CaseInsensitiveMap<T, U> extends Map<T, U> {
   get(key: T): U | undefined {
     if (typeof key === 'string') {
       for (const k of super.keys()) {
-        if (((k as unknown) as string).toLowerCase() === key.toLowerCase()) {
+        if ((k as unknown as string).toLowerCase() === key.toLowerCase()) {
           return super.get(k)
         }
       }
